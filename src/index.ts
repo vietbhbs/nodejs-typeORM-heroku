e58a8ccad9f1fb60556be2cbefe6231ece8e62eb
tiencntt2 <tiencntt2@gmail.com>
2022-08-25 07:02:48 +0700

    feat(auth): handle auth with jwt

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
+import 'reflect-metadata'
 import { AppDataSource } from './data-source'
-import { User } from './entity/User'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import helmet from 'helmet'
+import * as cors from 'cors'
+import routes from './routes'
 
 AppDataSource.initialize()
-  .then(async () => {
-    console.log('Inserting a new user into the database...')
-    const user = new User()
-    user.firstName = 'Timber'
-    user.lastName = 'Saw'
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log('Saved a new user with id: ' + user.id)
+    .then(async () => {
+        // Create a new express application instance
+        const app = express()
 
-    console.log('Loading users from the database...')
-    const users = await AppDataSource.manager.find(User)
-    console.log('Loaded users: ', users)
+        // Call middlewares
+        app.use(cors())
+        app.use(helmet())
+        app.use(bodyParser.json())
 
-    console.log(
-      'Here you can setup and run express / fastify / any other framework.',
-    )
-  })
-  .catch((error) => console.log(error))
+        //Set all routes from routes folder
+        app.use('/', routes)
+
+        app.listen(3000, () => {
+            console.log('Server started on port 3000!')
+        })
+    })
+    .catch((error) => console.log(error))
