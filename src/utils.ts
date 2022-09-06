62f3b5a01a0360423faad3345d76fb046c94f1e9
TienHM <tienhm@beetsoft.com.vn>
2022-09-06 16:23:26 +0700

    feat(user): format validate response

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { Response } from 'express'
+import config from './config/config'
 
 export default class Utils {
     /**
@@ -25,5 +26,82 @@ export default class Utils {
      */
     static getVersionNumber(version, prefix = 'v'): number {
         return version.substring(version.indexOf(prefix) + 1)
+    }
+
+    static formatAPIVersionNotMatchResponse() {
+        const response: string[] = []
+        response[config.exitCodeKey] = config.exitCode.apiVersionNotMatch
+
+        response[config.desKey] = config.message.apiVersionNotMatch
+
+        return Object.assign({}, response)
+    }
+    /**
+     * format error response
+     * @param errors
+     */
+    static formatErrorResponse(errors) {
+        const response: string[] = []
+        response[config.exitCodeKey] = config.exitCode.invalidParams
+
+        response[config.desKey] = []
+        for (const errorsKey in errors) {
+            const constraints = errors[errorsKey].constraints
+            response[config.desKey].push(constraints)
+        }
+
+        response[config.inputDataKey] = errors[0].target
+
+        return Object.assign({}, response)
+    }
+
+    /**
+     * format success response
+     * @param actionText
+     * @param data
+     */
+    static formatSuccessResponse(actionText, data) {
+        const response: string[] = []
+        response[config.exitCodeKey] = config.exitCode.success
+
+        response[config.desKey] = actionText + ' - ' + config.message.success
+
+        if (
+            actionText.includes(config.action.create) ||
+            actionText.includes(config.action.update)
+        ) {
+            response[config.insertIdKey] = data
+        } else {
+            response[config.dataKey] = data
+        }
+
+        return Object.assign({}, response)
+    }
+
+    /**
+     * format not exist record response
+     * @param data
+     */
+    static formatNotExistRecordResponse(data) {
+        const response: string[] = []
+        response[config.exitCodeKey] = config.exitCode.notFound
+
+        response[config.desKey] = config.message.notFound
+        response[config.inputDataKey] = data
+
+        return Object.assign({}, response)
+    }
+
+    /**
+     * format error signature response
+     * @param signature
+     */
+    static formatErrorSignatureResponse(signature: string) {
+        const response: string[] = []
+        response[config.exitCodeKey] = config.exitCode.invalidSignature
+        response[config.desKey] = config.message.invalidSignature
+        response[config.validKey] = signature
+
+        return Object.assign({}, response)
     }
 }
